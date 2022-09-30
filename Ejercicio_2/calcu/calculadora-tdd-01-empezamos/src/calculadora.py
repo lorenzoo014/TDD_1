831613fa2ad52bcb7a8f7abab00798fa26e0f411
lorenzoo22 <lagudmarr@gmail.com>
2022-09-30 14:33:21 +0200

    convirtiendo los directorios en modulos

---
+++ Ejercicio_2/calcu/calculadora-tdd-01-empezamos/src/calculadora.py
@@ -0,0 +1,8 @@
+# Cargamos el módulo unittest
+import unittest
+# Creamos una clase heredando de TestCase
+class TestMyCalculator(unittest.TestCase):
+ # Creamos una prueba para probar un valor inicial
+ def test_initial_value(self):
+ calc = Calculator()
+ self.assertEqual(0, calc.value)
